83c5d27951c5143c6685227bc6b6c1821cc539e1
Olaoye Salem <89555234+olaoyesalem@users.noreply.github.com>
2023-03-17 13:21:54 +0100

    Create 01_deploy-Lottery.js

--- deploy/01_deploy-Lottery.js
+++ deploy/01_deploy-Lottery.js
@@ -2,7 +2,7 @@ const { ethers,network } = require("hardhat")
 const { developmentChains, networkConfig } = require("../helper-hardhat-config")
 const{verify} = require("../utils/verify.js")
 
-//
+
 module.exports = async function ({getNamedAccounts, deployments}) {
   const VRF_FUND_AMOUNT = await ethers.utils.parseEther("1")
   const {deploy,log} = deployments
