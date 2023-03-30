8dfe0757201d0feee82379f06f3d0efdcdb59cb8
Olaoye Salem <89555234+olaoyesalem@users.noreply.github.com>
2023-03-30 15:16:06 -0700

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,6 +14,8 @@ const contract = await contractFactory.deploy(BASE_FEE,GAS_PRICE_LINK)
  await contract.deployed
  const contractAddress = contract.address
 console.log(contractAddress) 
+  
+  
 
 const subscription_id = contract.events[0]
 console.log(subscription_id)
