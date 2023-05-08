25bb9e158ddaeb36490c86d341c786788ba00c4f
olaoyesalem <olaoyesalemgreat@gmail.com>
2023-05-09 03:58:27 +1300

    justHardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -30,20 +30,20 @@ module.exports = {
 			default: 1,
 		},
 	},
-	networks: {
-		localHost: {
-			url: LOCAL_HOST_URL,
-			chainId: 31337,
-			accounts: [LOCAL_HOST_ACCOUNT],
-			blockConfirmations: 1,
-		},
-		sepolia: {
-			accounts: [SEPOLIA_ACCOUNT],
-			blockConfirmations: 6,
-			url: SEPOLIA_URL,
-			chainId: 11155111,
-		},
-	},
+	// networks: {
+	// 	localHost: {
+	// 		url: LOCAL_HOST_URL,
+	// 		chainId: 31337,
+	// 		accounts: [LOCAL_HOST_ACCOUNT],
+	// 		blockConfirmations: 1,
+	// 	},
+	// 	sepolia: {
+	// 		accounts: [SEPOLIA_ACCOUNT],
+	// 		blockConfirmations: 6,
+	// 		url: SEPOLIA_URL,
+	// 		chainId: 11155111,
+	// 	},
+	// },
 };
 
 // "@nomicfoundation/hardhat-network-helpers@^1.0.0" "@nomicfoundation/hardhat-chai-matchers@^1.0.0" "@nomiclabs/hardhat-ethers@^2.0.0" "@nomiclabs/hardhat-etherscan@^3.0.0" "@types/chai@^4.2.0" "@types/mocha@^9.1.0" "@typechain/ethers-v5@^10.1.0" "@typechain/hardhat@^6.1.2" "chai@^4.2.0" "hardhat-gas-reporter@^1.0.8" "solidity-coverage@^0.8.1" "ts-node@>=8.0.0" "typechain@^8.1.0"
